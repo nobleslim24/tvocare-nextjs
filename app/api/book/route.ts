89fb477282971ee64d0c87a21a2694aa14d08a49
essu christian <essuchristian@gmail.com>
2026-07-16 19:44:47 +0100

    Switched from StaticForms to Web3Forms

--- app/api/book/route.ts
+++ app/api/book/route.ts
@@ -6,26 +6,26 @@ export async function POST(request: Request) {
 
     console.log("Data received from booking form:", data);
 
-    const response = await fetch("https://api.staticforms.dev/submit", {
+    const response = await fetch("https://api.web3forms.com/submit", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        apiKey: process.env.STATICFORMS_API_KEY,
+        access_key: process.env.WEB3FORMS_ACCESS_KEY,
         ...data,
       }),
     });
 
     const result = await response.json();
 
-    console.log("StaticForms booking response:", result);
+    console.log("Web3Forms booking response:", result);
 
     return NextResponse.json(result, {
       status: response.status,
     });
   } catch (error) {
-    console.error("StaticForms booking error:", error);
+    console.error("Web3Forms booking error:", error);
 
     return NextResponse.json(
       {
